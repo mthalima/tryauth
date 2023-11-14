19c19a541c0c72dc51639e95c314ac1c0332bca4
mthalima <matheus.ecomp@gmail.com>
2023-11-14 19:26:11 -0300

    criado a autenticacao com token

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,15 +3,19 @@ import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 import { login } from "../util/auth";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
+  const authCtx = useContext(AuthContext);
+
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert("Falha de autenticação", "Login ou senha inválido.");
     }
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { createUser } from "../util/auth";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
+  const authCtx = useContext(AuthContext);
+
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert("Erro!", "Falha ao criar usuário");
     }
